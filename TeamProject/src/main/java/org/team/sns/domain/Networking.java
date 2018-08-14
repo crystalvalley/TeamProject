4e2cdb5f3c6956142c925a0a5fe6a24cc9ed8782
Unknown <minju.cha01@gmail.com>
2018-08-14 15:13:19 +0900

    table update

--- TeamProject/src/main/java/org/team/sns/domain/Networking.java
+++ TeamProject/src/main/java/org/team/sns/domain/Networking.java
@@ -19,28 +19,27 @@ import lombok.EqualsAndHashCode;
  * 
  * @author ChaMinju
  * @since 18.08.10
- * @version 18.08.11
+ * @version 18.08.14
  * 
  */
-
 
 @Data
 @Entity
 @Table(name = "Networking")
-@EqualsAndHashCode(of = "_id")
+@EqualsAndHashCode(of = "id")
 public class Networking {
 	
 	@Id
 	@Column(name = "Networking_id")
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
 	@SequenceGenerator(name = "seq", sequenceName = "Networking_seq", initialValue = 1, allocationSize = 1)
-	private int _id;	
+	private int id;	
 
 	@NotNull
 	private String type;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	@JoinColumn(name = "Owners", referencedColumnName = "user_id")
 	private Member member;
 	
 	@ManyToOne(cascade=CascadeType.ALL)
