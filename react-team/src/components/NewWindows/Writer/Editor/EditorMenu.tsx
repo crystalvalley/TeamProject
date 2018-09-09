13820a9b613bae1970689e1a620b3c37b987fcdd
ParkHyeokjoon <hamkegaja2@gmail.com>
2018-09-09 22:33:50 +0900

    recommit
    recommit

--- react-team/src/components/NewWindows/Writer/Editor/EditorMenu.tsx
+++ react-team/src/components/NewWindows/Writer/Editor/EditorMenu.tsx
@@ -1,20 +1,49 @@
 import * as React from 'react';
-import { StyleRulesCallback, Theme, IconButton, withStyles } from '@material-ui/core';
-import Save from '@material-ui/icons/Save';
+import { StyleRulesCallback, Theme, withStyles } from '@material-ui/core';
 import { EditorState } from 'draft-js';
+import Dropzone from 'react-dropzone';
 
 /**
  * @author : ParkHyeokJoon
  * @since : 2018.08.20
- * @version : 2018.08.23
+ * @version : 2018.09.09
  */
 
 const style: StyleRulesCallback = (theme: Theme) => ({
-
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        height: "100%"
+    },
+    buttons: {
+        display: "flex",
+        minHeight: "50px",
+        border: "1px solid black",
+        margin: "5px"
+    },
+    audio: {
+        display: "flex",
+        minHeight: "50px",
+        border: "1px solid black",
+        flexBasis: "10%",
+        margin: "5px"
+    },
+    photos: {
+        display: "flex",
+        minHeight: "50px",
+        flexGrow: 1,
+        border: "1px solid black",
+        margin: "5px",
+        flexDirection: "column"
+    }
 })
 
 interface IProps {
     classes: {
+        container: string;
+        photos: string;
+        buttons: string;
+        audio: string;
 
     }
     title: string;
@@ -22,18 +51,58 @@ interface IProps {
     writer: string;
 }
 
-class EditorMenu extends React.Component<IProps> {
+interface IState {
+    files: File[];
+}
+
+class EditorMenu extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
+        this.state = {
+            files: []
+        }
+        this.onDrop = this.onDrop.bind(this);
     }
     public render() {
+        const { classes } = this.props
         return (
-            <div>
-                <IconButton>
-                    <Save />
-                </IconButton>
+            <div
+                className={classes.container}
+            >
+                <div
+                    className={classes.buttons}
+                >
+                    Buttons
+                </div>
+                <div
+                    className={classes.audio}
+                >
+                    Audio
+                </div>
+                <div
+                    className={classes.photos}
+                >
+                    <Dropzone
+                        onDrop={this.onDrop}
+                    >
+                        <p>Drop here</p>
+                    </Dropzone>
+                    <aside>
+                        <h2>dropped files</h2>
+                        <ul>
+                            {
+                                this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+                            }
+                        </ul>
+                    </aside>
+                </div>
             </div>
         );
+    }
+    private onDrop(files: File[]) {
+        this.setState({
+            files
+        })
     }
 }
 
