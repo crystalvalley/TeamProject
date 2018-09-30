c938ff0fff5d1b48a4a04c694b07a63d1673cb61
Unknown <vzbsm13@gmail.com>
2018-09-30 18:38:43 +0900

    minjeong0930

--- react-team/src/components/Account/SignIn.tsx
+++ react-team/src/components/Account/SignIn.tsx
@@ -121,14 +121,14 @@ class SignIn extends React.Component<IProps & ILoginStore, IState> {
                             </Button>
 
                     </form>
-                        </form>
-                        <div className={classes.footer}>
+
+                    <div className={classes.footer}>
                         <Typography>
                             Powered By SCI
                         </Typography>
-                        </div>
+                    </div>
 
-                   
+
                 </div>
 
 
