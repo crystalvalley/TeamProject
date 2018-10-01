38c860ec53ec78577c874a96d407b36e9bdd6d53
ParkHyeokjoon <hamkegaja2@gmail.com>
2018-10-01 21:15:33 +0900

    111111

--- react-team/src/components/MainContainer/CardList/Card/bigCard/ReplyList.tsx
+++ react-team/src/components/MainContainer/CardList/Card/bigCard/ReplyList.tsx
@@ -68,7 +68,7 @@ class ReplyList extends React.Component<IProps & ILoginStore, IState>{
                 <div
                     className={classes.table}
                 >
-                    <Avatar src={ROOTURL+this.props.reply.writer.profileImg} />
+                    <Avatar src={ROOTURL+"/resources"+this.props.reply.writer.profileImg} />
                     <div
                         style={{
                             marginLeft: "10px",
